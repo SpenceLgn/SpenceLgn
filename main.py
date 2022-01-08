0cbb1cc0c0e994e1e52396b8067b11e3901ac322
SpenceLgn <97334077+SpenceLgn@users.noreply.github.com>
2022-01-08 14:02:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @SpenceLgn
+- 👀 I’m interested in Discord.py
+- 🌱 I’m currently learning python
+
+
+
+<!---
+SpenceLgn/SpenceLgn is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
